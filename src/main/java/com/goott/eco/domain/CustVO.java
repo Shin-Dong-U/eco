09fcd0e08b88b1d19f3caa19e3b72a95f5407bd0
pureum <kpr227@naver.com>
2021-03-02 20:34:01 +0900

    0302_kpr
    custVO modify

--- src/main/java/com/goott/eco/domain/CustVO.java
+++ src/main/java/com/goott/eco/domain/CustVO.java
@@ -2,28 +2,33 @@ package com.goott.eco.domain;
 
 import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CustVO {
-	private String memberId;	//
-	private String password;	//
-	private String name;		//
-	private String phone;       //
-	private String email;       //
-	private String addr_post;   //
-	private String addr_city;   //
-	private String addr_detail; //
-	private String birth;       //
-	private int sex;            //
-	private String company_yn;  //
-	private String member_yn;   //
-	private Timestamp regDate;  //
-	private String regUser;     //
-	private Timestamp editDate; //
-	private String editUser;    //
-	private String myTree;      //
-	private Long myMil;         //
-
+	private String memberId;	// 아이디
+	private String password;	// 비밀번호
+	private String name;		// 이름
+	private String phone;       // ex)01011112222
+	private String email;       // email 
+	private String addr_post;   // 우편번호
+	private String addr_city;   // 주소 도 , 시 , 동
+	private String addr_detail; // 주소 상세 정보
+	private String birth;       // 생년월일  ex)990101 
+	private Long sex;           // 성별 [1]-남자 [2]-여자
+	private String company_yn;  // 일반고객or회사 [y]-회사 [n]-일반사용자
+	private String member_yn;   // 회원탈퇴 여부
+	private Timestamp regDate;  // 가입 날짜
+	private String regUser;     // 가입한 회원
+	private Timestamp editDate; // 수정 날짜
+	private String editUser;    // 수정한 회원
+	private String myTree;      // Tree 정보 [A]-새싹 [E]-나무
+	private Long myMil;         // Mileage정보 
+	private Long bar_status;	// Mileage상태 bar 0~9
+	private String admin_yn;	// 일반 사용자 or 관리자 [y]-admin [n]-user
 }
--- src/main/java/com/goott/eco/mapper/CustMapper.java
+++ src/main/java/com/goott/eco/mapper/CustMapper.java
@@ -5,19 +5,27 @@ import java.util.Map;
 
 import com.goott.eco.domain.CustVO;
 
+
 public interface CustMapper {
 	
 	public List<Map<String, Object>> getCustList(Map<String, Object> searchInfo);
 	
 	//public String getId(String id);
 	public CustVO getCustId(CustVO custVO);  
+
+	/* 특정 회원정보 가져오기 */
+	public CustVO getCust(String memberId);
 	
-
-
 	/* 로그인  */
 	public CustVO getCustLogin(CustVO custVO);
 	
-	/*회원가입*/
+	/* 회원 가입 */
 	public int joinCust(CustVO custVO);
-
+	
+	/* 회원 수정 */
+	public int modifyCust(CustVO custVO);
+	
+	/* 회원 삭제 */
+	public int deleteCust(String memberId);
+	
 }
